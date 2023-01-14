20a955848cf27942bd990e22405f8cf08c63f441
Piotr <Piotr@DESKTOP-OGB5VIU>
2023-01-14 21:05:12 +0100

    + get total sum endpoint (month/year)

--- routes/receipt.js
+++ routes/receipt.js
@@ -1,18 +1,32 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const ReceiptController = require('../controllers/ReceiptController')
-const authenticate = require('../middleware/authenticate')
+const ReceiptController = require("../controllers/ReceiptController");
+const authenticate = require("../middleware/authenticate");
 
-router.get('/:userId', authenticate, ReceiptController.show)
+router.get("/:userId", authenticate, ReceiptController.show);
 
-router.post('/:userId', authenticate, ReceiptController.store)
-router.post('/item/:receiptId', authenticate, ReceiptController.storeItem)
+router.post("/:userId", authenticate, ReceiptController.store);
+router.post("/item/:receiptId", authenticate, ReceiptController.storeItem);
 
-router.patch('/:receiptId', authenticate, ReceiptController.update)
-router.patch('/:receiptId/item/:itemId', authenticate, ReceiptController.updateItem)
+router.patch("/:receiptId", authenticate, ReceiptController.update);
+router.patch(
+  "/:receiptId/item/:itemId",
+  authenticate,
+  ReceiptController.updateItem
+);
 
-router.delete('/:receiptId', authenticate, ReceiptController.destroy)
-router.delete('/:receiptId/item/:itemId', authenticate, ReceiptController.destroyItem)
+router.delete("/:receiptId", authenticate, ReceiptController.destroy);
+router.delete(
+  "/:receiptId/item/:itemId",
+  authenticate,
+  ReceiptController.destroyItem
+);
 
-module.exports = router
+router.get(
+  "/:userId/total/:month/:year",
+  authenticate,
+  ReceiptController.getMonthTotalSum
+);
+
+module.exports = router;
